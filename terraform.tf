9afe2567b2ceaf045163bdd9ab14624cdffb7b42
xpcosmos <mikeias.d.s.o@gmail.com>
2025-03-08 15:19:01 -0300

    Configurando Bucket de testes dentro do Terraform

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,26 @@
 terraform {
-    required_providers {
-        google = {
-            source = "hashicorp/google"
-        }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
     }
+  }
 }
 
 provider "google" {
-    project = "test-dev--mikeias-d-s-o"
-    region = "us-central1"
+  project = "test-dev--mikeias-d-s-o"
+  region  = "us-central1"
+}
+resource "google_storage_bucket" "dev-bucket" {
+  name          = "test-dev--mikeiascamp--dev-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
